115eb14301b9317b820a9890303057289f7575eb
ahmedmedht <ahmed.m.mehisen@gmail.com>
2023-03-26 02:00:11 +0200

    quiz edit md7t lol

--- app/src/main/java/com/example/quizproject/HomePages/JoinFragment.kt
+++ app/src/main/java/com/example/quizproject/HomePages/JoinFragment.kt
@@ -1,25 +1,26 @@
 package com.example.quizproject.HomePages
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.navArgs
-import com.example.quizproject.Model.QuestionModel
 
 import com.example.quizproject.R
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.fragment_join_code.*
+import kotlinx.android.synthetic.main.fragment_join.*
 import com.google.firebase.database.FirebaseDatabase as FirebaseDatabase
 
 class JoinFragment : Fragment() {
 
     private lateinit var dref: DatabaseReference
-    private lateinit var getQuizQuestion:ArrayList<QuestionModel>
+    private var getQuizQuestion:DataSnapshot?=null
     private val args:JoinFragmentArgs by navArgs()
-    private lateinit var data: FirebaseDatabase
-    private val getCodeQuizJoin=args.getCodeQuiz
+    private var result=0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,26 +33,61 @@ class JoinFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val getCodeQuizJoin=args.getCodeQuiz
         dref= FirebaseDatabase.getInstance().getReference("QuizApp")
-        data=FirebaseDatabase.getInstance()
-        getQuizQuestion=ArrayList()
+        var numberquestion=1
 
-        getDataFromFirebase()
+        getDataFromFirebase(getCodeQuizJoin)
 
-    }
+        //showfirstquestion()
 
-    private fun getDataFromFirebase() {
-        dref.child(getCodeQuizJoin).get().addOnSuccessListener {
-            if (it.exists()){
 
+
+        btn_next_quiz_join.setOnClickListener {
+            if(getQuizQuestion?.hasChild(numberquestion.toString()) == true){
+                val qeustion=getQuizQuestion?.child(numberquestion.toString())?.child("question")?.value.toString()
+                txt_question_join.text = qeustion
+                val answer=getQuizQuestion?.child("0")?.child("arrayanswr")
+                txt_answer1.text = answer?.child("0")?.value.toString()
+                txt_answer2.text = answer?.child("1")?.value.toString()
+                txt_answer3.text = answer?.child("2")?.value.toString()
+                txt_answer4.text = answer?.child("3")?.value.toString()
+                numberquestion += 1
             }else{
 
             }
+        }
+
+    }
+    private fun getDataFromFirebase(getCodeQuizJoin: String) {
+        dref.child("Quizes").child(getCodeQuizJoin).get().addOnSuccessListener {
+            if (it.exists()){
+
+                getQuizQuestion=it
+
+                Log.d("arraytest",getQuizQuestion.toString())
+
+            }else{
+                Toast.makeText(context,"QUIZ DOESN'T EXISTS ",Toast.LENGTH_LONG).show()
+
+            }
         }.addOnFailureListener {
-            
+            Toast.makeText(context,"getQuizQuestion.toString()",Toast.LENGTH_LONG).show()
+
         }
     }
+
+    private fun showfirstquestion() {
+        val qeustion=getQuizQuestion?.child("0")?.child("question")?.value.toString()
+        txt_question_join.text = qeustion
+        val answer=getQuizQuestion?.child("0")?.child("arrayanswr")
+        txt_answer1.text = answer?.child("0")?.value.toString()
+        txt_answer2.text = answer?.child("1")?.value.toString()
+        txt_answer3.text = answer?.child("2")?.value.toString()
+        txt_answer4.text = answer?.child("3")?.value.toString()
+    }
+
+
 
 
 
@@ -59,3 +95,5 @@ class JoinFragment : Fragment() {
 
 
 }
+
+
